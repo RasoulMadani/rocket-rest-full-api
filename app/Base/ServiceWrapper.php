40e43b6c284a71263fc41a41d87d81e960480f6a
Rasoulmadani <ali.rahbary@chmail.ir>
2024-03-21 15:17:41 +0330

    جداسازی کدهای تکراری  در لایه سرویس و قرار دادن آن در رپر

---
+++ app/Base/ServiceWrapper.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Base;
+
+use Illuminate\Contracts\Debug\ExceptionHandler;
+
+class ServiceWrapper
+{
+    public function __invoke(\Closure $action, \Closure $reject = null)
+    {
+        try {
+            $actionResult = $action();
+        } catch (\Throwable $th) {
+
+            !is_null($reject) && $reject();
+
+            // دریافت خطا در تلسکوپ
+            app()[ExceptionHandler::class]->report($th);
+
+            return new ServiceResult(false, $th->getMessage());
+        }
+
+        return new ServiceResult(true, $actionResult);
+    }
+}
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,26 +3,20 @@
 namespace App\Services;
 
 use App\Base\ServiceResult;
+use App\Base\ServiceWrapper;
 use App\Models\User;
-use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Support\Facades\Hash;
 
 class UserService
 {
     public function registerUser(array $inputs):ServiceResult
     {
-        try {
+        return app(ServiceWrapper::class)(function () use ($inputs){
+
             $inputs['password'] = Hash::make($inputs['password']);
+            return User::create($inputs);
+        });
+      
 
-            $user = User::create($inputs);
-
-        } catch (\Throwable $th) {
-             // دریافت خطا در تلسکوپ
-             app()[ExceptionHandler::class]->report($th);
-
-             return new ServiceResult(false,$th->getMessage());
-        }
-
-        return new ServiceResult(false,$user);
     }
 }
