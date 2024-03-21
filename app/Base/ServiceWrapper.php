43dfa55f9d3e21dc8550853531bb7810321a89a4
Rasoulmadani <ali.rahbary@chmail.ir>
2024-03-21 15:22:25 +0330

    اضافه کردن ترنزاکشن به رپر سرویس

--- app/Base/ServiceWrapper.php
+++ app/Base/ServiceWrapper.php
@@ -3,15 +3,18 @@
 namespace App\Base;
 
 use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Support\Facades\DB;
 
 class ServiceWrapper
 {
     public function __invoke(\Closure $action, \Closure $reject = null)
     {
+        DB::beginTransaction();
         try {
             $actionResult = $action();
+            DB::commit();
         } catch (\Throwable $th) {
-
+            DB::rollBack();
             !is_null($reject) && $reject();
 
             // دریافت خطا در تلسکوپ
